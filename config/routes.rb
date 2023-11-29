b9abb5ed5c78a9f0f84d4acc6fa901f99564b5a3
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-11-29 11:42:06 +0000

    [Add]public/homesページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  scope module: 'public' do
   root to: 'homes#top'
   get '/about' => "homes#about", as: 'about'
+  end
 
   namespace :admin do
     get 'homes/top'
