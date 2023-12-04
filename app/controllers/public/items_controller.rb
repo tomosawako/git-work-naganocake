3ea78834826a79fb05f4d49058e35edca803bd71
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-04 07:40:54 +0000

    [Add]動的なヘッダー

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,8 @@
 class Public::ItemsController < ApplicationController
+
+  has_one_attached :image
+  belongs_to :cart_item
+  belongs_to :order_detail
+  has_many :genre, dependent: :destroy
+
 end
