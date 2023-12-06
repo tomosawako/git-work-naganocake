8ebee27cc0b2491a11f6ca88415a303cbe2fa2ef
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-06 04:08:34 +0000

    [Update]商品登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,4 +7,16 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def cerate
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price)
+  end
+
 end
