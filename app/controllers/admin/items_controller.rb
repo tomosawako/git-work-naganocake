0968b411212ea4f6d9540826aec13f918cd67654
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-06 01:32:26 +0000

    [Start]商品登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,10 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
     resources :genres, only: [:index, :create, :edit ,:update]
+    resources :items, except: [:destroy]
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
