f75a97769f82a48a67657c509b0f9909145dc9f7
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-19 13:03:56 +0000

    [Add]会員：注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -47,7 +47,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def completed
