ff5373d5e70d3687fcd9538e6098e3ea9aab97a6
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-20 07:23:12 +0000

    [Start]ステータス機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,4 +4,10 @@ class Admin::OrdersController < ApplicationController
     @order_details = @order.order_details
     @sum = 0
   end
+
+  def update
+    order = Order.find(params[:id])
+    order.update
+    redirect_back(fallback_location: admin_root_path)
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,6 +3,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+  enum making_status: { unable_start: 0, waiting_production: 1, making: 2, finish: 3 }
+
   def subtotal
     item.with_tax_price * amount
   end
