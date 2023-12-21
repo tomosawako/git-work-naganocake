2ad6aee5034e73ee919039cbeba9da012144b67f
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-21 02:45:33 +0000

    [Add]注文ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,7 +7,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    order.update
-    redirect_back(fallback_location: admin_root_path)
+    order.update(order_params)
+    redirect_back(fallback_location: admin_homes_top_path)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :customers, only: [:index, :show, :edit ,:update]
     resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
