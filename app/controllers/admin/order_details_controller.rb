83c45b321c4b197ba2983927da565aeddd1d5181
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-12-21 13:18:02 +0000

    [Add]制作ステータス機能

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,11 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     order_detail = OrderDetail.find(params[:id])
-    @order = Order.find(params[:id])
+    @order = Order.find_by(id: order_detail.order_id)
     order_detail.update(order_detail_params)
+    if @order.order_details.all?{|detail|detail.making_status=="finish"}
+      @order.update(status:"preparing_ship")
+    end
     redirect_back(fallback_location: admin_homes_top_path)
   end
 
